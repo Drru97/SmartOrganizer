eb46542919801b8526aba5c75736fe0595c43bb5
Drru97 <oleg.dystak@gmail.com>
2018-04-23 10:12:17 +0300

    Add tests project.

---
+++ test/Services/SmartOrganizer.Services.Tests/AppointmentServiceTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Moq;
+using Xunit;
+
+namespace SmartOrganizer.Services.Tests
+{
+    public class AppointmentServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
